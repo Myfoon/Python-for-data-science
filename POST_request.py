faf95982a4a1efee3eff1b4a44592fd8933bdde4
Myfoon <98499037+Myfoon@users.noreply.github.com>
2024-03-04 18:27:44 +0200

    Add files via upload

---
+++ POST_request.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+
+my_personal_dict = {"name":"Viacheslav","surname":"Ivanov","contact":[{"email":"mafon.ff@gmail.com"},{"tg":None}]}
+
+
+
+
+print(json.dumps(my_personal_dict))
+
+
+print(type(json.dumps(my_personal_dict)))
+
+response = requests.post("https://bot-besample.app/webhook/ed0c4fe4-ea61-4402-b50f-95db8602f97c",
+                         json = my_personal_dict,
+                         headers = {"Authorization": "8BrOGc5DXA8e3N5F11Efqxd5b8Fc3Hpz"})
+
+print(response.text)
