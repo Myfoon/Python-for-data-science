876763fc0c92e24e8ea3092f259c796a99dd8287
Myfoon <98499037+Myfoon@users.noreply.github.com>
2024-05-03 18:27:57 +0300

    Update POST_request.py

--- POST_request.py
+++ POST_request.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 
-my_personal_dict = {"name":"Viacheslav","surname":"Ivanov","contact":[{"email":"dummy.mail@gmail.com"},{"tg":None}]}
+my_personal_dict = {"name":"Peres","surname":"Kibaev","contact":[{"email":"dummy.mail@gmail.com"},{"tg":None}]}
 
 
 
